940d143f744992b233be3e856b375ac72cb9adda
Gauravbhatia1211 <68150602+Gauravbhatia1211@users.noreply.github.com>
2024-09-30 23:03:21 +0530

    Update scans3.sh

--- scans3.sh
+++ scans3.sh
@@ -13,11 +13,8 @@ search_s3_buckets() {
     local org="$1"
     echo "Searching for 's3.amazonaws.com' in organization: $org"
 
-    # Initialize a variable for total found
-    total_buckets=0
-
     # Perform a search for the S3 bucket pattern
-    search_results=$(gh api search/code -q "s3.amazonaws.com in:file org:$org" --json items -q '.items[] | .repository.full_name + " | " + .path')
+    search_results=$(gh search code --query "s3.amazonaws.com in:file org:$org" --limit 100)
 
     # Check if any results were found
     if [ -z "$search_results" ]; then
@@ -26,9 +23,7 @@ search_s3_buckets() {
         echo "🚨 **S3 Buckets Found** 🚨"
         echo "$search_results" | while read -r line; do
             echo "- $line"
-            ((total_buckets++))
         done
-        echo "**Total S3 Buckets Found:** $total_buckets"
     fi
 }
 
